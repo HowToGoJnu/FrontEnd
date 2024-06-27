1fe1c2f45cf48604cf8c3dbd16e583f1bd2f4b85
minjung <k1950035@naver.com>
2024-06-28 04:48:41 +0900

    Feat/MainScreen and StartScreen
    StartScreen페이지를 작업했으며, MainScreen의 시간표 기능을 완성중입니다.

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,11 +1,255 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Pressable,
+  ScrollView,
+  StatusBar,
+  Modal,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import CalendarIcon from '../icons/icon_calendar.svg';
+import AlarmIcon from '../icons/icon_alarm.svg';
+import {SubjectType} from '../../types';
 
 function MainScreen() {
+  const days = ['월', '화', '수', '목', '금'];
+  const colors = [
+    'pink',
+    'green',
+    'blue',
+    'darkblue',
+    'yellow',
+    'brown',
+    'purple',
+  ];
+  const hours = [9, 10, 11, 12, 1, 2, 3, 4];
+  const [modalVisible, setModalVisible] = useState(false);
+  const initialData: SubjectType = {
+    Subject: '',
+    Building: '',
+    StartTime: '',
+    EndTime: '',
+    Day: '',
+    Color: '',
+  };
+  const [subjectData, setSubjectData] = useState<SubjectType>(initialData);
+  const [schedule, setSchedule] = useState<SubjectType[]>([]);
+
+  function clearSubject() {
+    setSubjectData(initialData);
+  }
+
+  function addSubjectToSchedule() {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    const newSubject = {...subjectData, Color: randomColor};
+
+    setSchedule([...schedule, newSubject]);
+    setModalVisible(false);
+    clearSubject();
+  }
+
+  function renderSubjects(day: string, hour: number) {
+    return schedule
+      .filter(subject => {
+        const startTime = parseInt(subject.StartTime);
+        const endTime = parseInt(subject.EndTime);
+        return subject.Day === day && hour >= startTime && hour < endTime;
+      })
+      .map((subject, i) => {
+        const startTime = parseInt(subject.StartTime);
+        const endTime = parseInt(subject.EndTime);
+        const height = ((endTime - startTime) * 100).toString();
+        console.log(i);
+        return (
+          <View
+            key={i}
+            style={{backgroundColor: subject.Color, height: height}}
+            className="absolute inset-x-0 top-0 w-full justify-center items-center">
+            <Text className="text-center text-white">
+              {subject.Subject} ({subject.Building})
+            </Text>
+          </View>
+        );
+      });
+  }
+
   return (
-    <View>
-      <Text>메인페이지</Text>
-    </View>
+    <SafeAreaView>
+      <StatusBar barStyle="default" />
+      <View className="flex w-screen h-screen items-center bg-white">
+        <View className="flex w-11/12 h-[60px] justify-end">
+          <Text className="font-inter-r text-primary-green">
+            2024년 여름 학기
+          </Text>
+          <View className="flex flex-row w-full h-auto justify-between items-center">
+            <View className="flex flex-row w-auto h-auto justify-center items-center">
+              <CalendarIcon width={20} height={20} />
+              <Text className="font-inter text-xl text-black mx-1">시간표</Text>
+            </View>
+            <Pressable>
+              <AlarmIcon width={30} height={20} />
+            </Pressable>
+          </View>
+        </View>
+        <View className="w-11/12 h-[10px]" />
+        <View className="flex flex-grow w-11/12 h-full">
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <View className="flex w-full h-auto mb-20 bg-white">
+              <View className="flex w-auto h-auto border border-gray-300 rounded-xl">
+                {/* Header Row */}
+                <View className="flex flex-row justify-between items-center border-b border-gray-300">
+                  <View className="w-1/6 p-2">
+                    <Text className="text-center">시간</Text>
+                  </View>
+                  {days.map((day, index) => (
+                    <View key={index} className="w-1/6 p-2">
+                      <Text className="text-center">{day}</Text>
+                    </View>
+                  ))}
+                </View>
+                {hours.map((hour, rowIndex) => (
+                  <View
+                    key={rowIndex}
+                    className="flex flex-row justify-between items-center border-b border-gray-300 relative">
+                    <View className="w-1/6 p-2">
+                      <Text className="text-center">{hour}</Text>
+                    </View>
+                    {days.map((day, colIndex) => (
+                      <View
+                        key={colIndex}
+                        className="w-1/6 h-16 border-l border-gray-300 relative">
+                        {renderSubjects(day, hour)}
+                      </View>
+                    ))}
+                  </View>
+                ))}
+              </View>
+              <View className="flex w-full h-[10px] bg-white" />
+              <View className="flex w-full h-[150px] bg-white border border-gray-300 rounded-lg justify-center items-center">
+                <View className="flex flex-col w-full h-auto items-center">
+                  <Text className="font-inter text-lg mb-2">
+                    시간표 및 장소 추가
+                  </Text>
+                  <Pressable
+                    onPress={() => {
+                      setModalVisible(true);
+                    }}
+                    className="flex w-2/5 h-auto bg-primary-green rounded-2xl shadow-md shadow-black/50">
+                    <View className="flex flex-row w-full h-auto items-center justify-between px-4 py-1">
+                      <Text className="font-inter text-sm text-white">
+                        시간표 및 장소
+                      </Text>
+                      <Text className="font-inter text-lg text-white">+</Text>
+                    </View>
+                  </Pressable>
+                </View>
+              </View>
+            </View>
+          </ScrollView>
+        </View>
+        <Modal
+          animationType="slide"
+          transparent
+          visible={modalVisible}
+          onRequestClose={() => {
+            setModalVisible(!modalVisible);
+          }}>
+          <View className=" bg-black/75 h-screen w-screen items-center justify-center">
+            <View className="w-4/5 h-3/5 bg-white rounded-lg items-center justify-between px-4 py-8 shadow-lg shadow-black">
+              <Text className="text-left font-inter text-sm text-black">
+                수업 이름, 장소, 요일, 시간을 입력해주세요
+              </Text>
+              <View className="flex w-full h-4/5">
+                <View className="flex flex-row w-full h-1/4 items-center">
+                  <Text className="font-inter-r text-black">과목명</Text>
+                  <View className="flex w-3/4 h-2/3 px-1 border-b border-black/50">
+                    <TextInput
+                      className="font-inter-r text-black"
+                      placeholder="수업 이름"
+                      onChangeText={text =>
+                        setSubjectData({...subjectData, Subject: text})
+                      }
+                    />
+                  </View>
+                </View>
+                <View className="flex flex-row w-full h-1/4 items-center">
+                  <Text className="font-inter-r text-black">건물명</Text>
+                  <View className="flex w-3/4 h-2/3 px-1 border-b border-black/50">
+                    <TextInput
+                      className="font-inter-r text-black"
+                      placeholder="건물 이름(호관 기재)"
+                      onChangeText={text =>
+                        setSubjectData({...subjectData, Building: text})
+                      }
+                    />
+                  </View>
+                </View>
+                <View className="flex flex-row w-full h-1/5 items-center">
+                  <Text className="font-inter-r text-black">요일{'   '}</Text>
+                  <View className="flex w-3/4 h-2/3 px-1 border-b border-black/50">
+                    <TextInput
+                      className="font-inter-r text-black"
+                      placeholder="요일 (ex.월)"
+                      onChangeText={text =>
+                        setSubjectData({...subjectData, Day: text})
+                      }
+                    />
+                  </View>
+                </View>
+                <View className="flex flex-row w-full h-1/5 items-center">
+                  <Text className="font-inter-r text-black">시간{'   '}</Text>
+                  <View className="flex flex-row w-3/4 justify-between">
+                    <View className="flex w-2/5 h-2/3 px-1 border-b border-black/50">
+                      <TextInput
+                        className="font-inter-r text-black"
+                        placeholder="시작시간"
+                        onChangeText={text =>
+                          setSubjectData({
+                            ...subjectData,
+                            StartTime: text,
+                          })
+                        }
+                      />
+                    </View>
+                    <View className="flex w-2/5 h-2/3 px-1 border-b border-black/50">
+                      <TextInput
+                        className="font-inter-r text-black"
+                        placeholder="종료시간"
+                        onChangeText={text =>
+                          setSubjectData({...subjectData, EndTime: text})
+                        }
+                      />
+                    </View>
+                  </View>
+                </View>
+              </View>
+              <View className="flex-row justify-between w-full">
+                <TouchableOpacity
+                  className="rounded-md bg-primary-green w-[45%] py-2 justify-center items-center"
+                  onPress={addSubjectToSchedule}>
+                  <Text className="text-white font-inter-sb text-s text-center">
+                    완료
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  className="rounded-md bg-primary-green w-[45%] py-2 justify-center items-center"
+                  onPress={() => {
+                    clearSubject();
+                    setModalVisible(false);
+                  }}>
+                  <Text className="text-white font-inter-sb text-s text-center">
+                    취소
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </Modal>
+      </View>
+    </SafeAreaView>
   );
 }
 
--- src/screens/StartScreen.tsx
+++ src/screens/StartScreen.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
-import {View, Button} from 'react-native';
+import {SafeAreaView, View, Pressable, Text, StatusBar} from 'react-native';
 import {RootStackParamList} from '../../types';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import StoneIcon from '../icons/icon_stoneGranpa.svg';
 
 type StartScreenProps = NativeStackScreenProps<RootStackParamList, 'Start'>;
 
 function StartScreen({navigation}: StartScreenProps) {
   return (
-    <View>
-      <Button title="메인페이지" onPress={() => navigation.navigate('Main')} />
-    </View>
+    <SafeAreaView>
+      <StatusBar barStyle="default" />
+      <View className="flex w-screen h-screen bg-white items-center">
+        <View className="flex w-11/12 h-4/5 items-center justify-start">
+          <View className="flex w-full h-2/3 items-center justify-end">
+            <View className="flex flex-wrap flex-col w-auto h-auto items-center justify-center">
+              <StoneIcon width={130} height={130} />
+              <Text className="font-inter-r text-black/80 text-[14px]">
+                제주대학교 전용 시간표 어플리케이션
+              </Text>
+              <Text className="font-inter text-primary-green text-[30px]">
+                어떵가코
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View className="flex w-11/12 h-1/5 bg-white items-center">
+          <Pressable
+            className="flex w-11/12 h-1/3 justify-center items-center rounded-lg bg-primary-green shadow-lg shadow-black/100"
+            onPress={() => navigation.navigate('Main')}>
+            <Text className="font-inter text-lg text-white">시작하기</Text>
+          </Pressable>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
